faed193d33f394e2156ade818568f60cdef94087
ShahzadNarai <shahzadnarainarai@gmail.com>
2024-10-01 22:28:32 +0500

    new shahzad dude

---
+++ components/Marquee.js
@@ -0,0 +1,142 @@
+import React from "react";
+
+const Marquee = () => {
+    return (
+        <div className="app-marquee-container1 mb-10">
+            <div className="marquee-container1">
+                <div className="marquee-subcontainer1 text-white py-4 ">
+                    <div className="marquee-subitem-container1">
+                        <ul className="uli border">
+                            <li className="lii">
+                                <span className="spann">Web Developer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Web Designer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">UI/UX Designer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Software Developer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Web Developer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Web Designer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">UI/UX Designer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Software Developer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Web Developer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Web Designer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">UI/UX Designer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Software Developer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Web Developer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Web Designer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">UI/UX Designer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Software Developer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Web Developer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Web Designer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">UI/UX Designer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Software Developer</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="marquee-subitem-container1 ">
+                        <ul className="uli">
+                            <li className="lii">
+                                <span className="spann">Web Developer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Web Designer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">UI/UX Designer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Software Developer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Web Developer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Web Designer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">UI/UX Designer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Software Developer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Web Developer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Web Designer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">UI/UX Designer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Software Developer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Web Developer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Web Designer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">UI/UX Designer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Software Developer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Web Developer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Web Designer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">UI/UX Designer</span>
+                            </li>
+                            <li className="lii">
+                                <span className="spann">Software Developer</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Marquee;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import localFont from "next/font/local";
+import Link from "next/link";
+import Marquee from "@/components/Marquee";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -14,102 +16,201 @@ const geistMono = localFont({
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className={`${geistSans.variable} ${geistMono.variable} fix-bg`}>
+      <div className="Ellipse1"></div>
+      <div className="Ellipse2"></div>
+      <div className="container">
+        <button className="menu-toggle-button">Menu btn</button>
+        <div className="side-menu">
+          menu
+          <div className="profile-img-main"></div>
+          <div className="menu-list-main">
+            <ul className="mb-4">
+              <li className="border-t border-gray-500 py-2.5 px-5">
+                <Link
+                  href={"/"}
+                  style={{
+                    color: "var(--8, #909090)",
+                    fontFamily: "DM Sans",
+                    fontStyle: "normal",
+                    lineHeight: "24px",
+                  }}
+                  className="flex items-center gap-3 list-none text-base font-medium text-[#909090]"
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="border-t border-gray-500 py-2.5 px-5">
+                <Link
+                  href={"/"}
+                  style={{
+                    color: "var(--8, #909090)",
+                    fontFamily: "DM Sans",
+                    fontStyle: "normal",
+                    lineHeight: "24px",
+                  }}
+                  className="flex items-center gap-3 list-none text-base font-medium text-[#909090]"
+                >
+                  About
+                </Link>
+              </li>
+              <li className="border-t border-gray-500 py-2.5 px-5">
+                <Link
+                  href={"/"}
+                  style={{
+                    color: "var(--8, #909090)",
+                    fontFamily: "DM Sans",
+                    fontStyle: "normal",
+                    lineHeight: "24px",
+                  }}
+                  className="flex items-center gap-3 list-none text-base font-medium text-[#909090]"
+                >
+                  Resume
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div id="wrap">
+            <Link href="/">Download Cv</Link>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="main-containe">
+          <section className="section-one overflow-hidden">
+            <div className="row">
+              <div className="flex flex-col w-1/2">
+                <h2 className="text-[80px] leading-[90px]">Shahzad Ali</h2>
+                <h3 className="text-[24px] font-medium leading-9 py-5 text-[#ff759c]">
+                  MERN/Stack Developer
+                </h3>
+                <p className="best">
+                  We appreciate your trust greatly our clients choose us & our
+                  products because they know we are the best.
+                </p>
+                <div className="section-one-btns-main">
+                  <div className="wrapper">
+                    <Link href={"/#"} className="btn-hover">
+                      View Work
+                    </Link>
+                  </div>
+                  <div className="wrapper">
+                    <Link href={"/#contact"} className="btn-hover btn-hover2">
+                      Contact Me
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col w-1/2 relative">
+                <Image
+                  className="flower"
+                  alt="flower"
+                  src={"/download.png"}
+                  width={37}
+                  height={37}
+                />
+                <Image
+                  className="circular-img"
+                  src={"/visual.png"}
+                  alt="circular-img"
+                  width={120}
+                  height={120}
+                />
+                <Image
+                  className="jessica-main-img "
+                  src={"/jessica.png"}
+                  width={434}
+                  height={434}
+                />
+                <div className="worked-box">
+                  <p class="worked-more mb-4">
+                    Worked with more than 100 people
+                  </p>
+                  <div className="client-img-main relative">
+                    <Image
+                      src={"/client.jfif"}
+                      width={40}
+                      height={40}
+                      className="client-img relative"
+                    />
+                    <Image
+                      src={"/client.jfif"}
+                      width={40}
+                      height={40}
+                      className="client-img client-img2"
+                    />
+                    <Image
+                      src={"/client.jfif"}
+                      width={40}
+                      height={40}
+                      className="client-img client-img3"
+                    />
+                    <Image
+                      src={"/client.jfif"}
+                      width={40}
+                      height={40}
+                      className="client-img client-img4"
+                    />
+                    <p className="worked-more worked-more2">100+ Clients</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section className="">
+            <Marquee />
+          </section>
+            <section class="About-section jos" id="about">
+              <div class="heading-container">
+                <h2 class="section-heading-text about-me">
+                  About Me.
+                </h2>
+                <div class="line"></div>
+              </div>
+              <p class="section-sub-text about-sub-text mb-4">
+                Hi, my name is <span>Shahzad Ali</span> and I began using
+                React.Js when first began. I've spent most of my waking hours
+                for the last ten years designing, programming and operating
+                React.Js sites go beyond with exclusive designer. Apart from
+                this I love to travel, mentor designers, review design
+                portfolios &amp; read books on everything related to design. I
+                have also given design talks in various educational
+                institutions. So I love creating creative content, and you can
+                find most of my works here.
+              </p>
+              <div class="about-detail-main">
+                <p class="about-detail">Name</p>
+                <p class="about-detail-info">Shahzad Ali</p>
+              </div>
+              <div class="about-detail-main">
+                <p class="about-detail">Nationality</p>
+                <p class="about-detail-info">Pakistan</p>
+              </div>
+              <div class="about-detail-main">
+                <p class="about-detail">Phone</p>
+                <p class="about-detail-info email">(+92) 0331 3666 399</p>
+              </div>
+              <div class="about-detail-main">
+                <p class="about-detail">Email</p>
+                <p class="about-detail-info email">shahzadnarainarai@gmail.com</p>
+              </div>
+              <div class="about-detail-main">
+                <p class="about-detail">Experience</p>
+                <p class="about-detail-info">1.5+ years</p>
+              </div>
+              <div class="about-detail-main">
+                <p class="about-detail">Freelance</p>
+                <p class="about-detail-info">Available</p>
+              </div>
+              <div class="about-detail-main">
+                <p class="about-detail">Skype</p>
+                <p class="about-detail-info">shahzadalinarai@hotmail.com</p>
+              </div>
+              <div class="about-detail-main">
+                <p class="about-detail">Language</p>
+                <p class="about-detail-info">English</p>
+              </div>
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
