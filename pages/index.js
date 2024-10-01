04047184d5950d861d027aca11c8b782ce54d8f3
ShahzadNarai <shahzadnarainarai@gmail.com>
2024-10-01 22:43:24 +0500

    latest

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ export default function Home() {
             </ul>
           </div>
           <div id="wrap">
-            <Link href="/">Download Cv</Link>
+            <Link className="btn-slide" href="/"> <span className="circle"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path class="download-svg" d="M13 12H16L12 16L8 12H11V8H13V12ZM15 4H5V20H19V8H15V4ZM3 2.9918C3 2.44405 3.44749 2 3.9985 2H16L20.9997 7L21 20.9925C21 21.5489 20.5551 22 20.0066 22H3.9934C3.44476 22 3 21.5447 3 21.0082V2.9918Z" fill="white"></path></svg></span> <span className="title">Download Cv</span> </Link>
           </div>
         </div>
         <div className="main-containe">
